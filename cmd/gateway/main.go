ac2c2ac8bbd120a39ebc15d12d5cb027e93c8399
DeFault <vadimfedorov753@gmail.com>
2026-07-31 19:44:50 +0300

    Add graceful shutdown

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -5,8 +5,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -48,9 +53,36 @@ func main() {
 
 	log.Println("server started on :8080")
 
-	err = server.ListenAndServe()
+	go func() {
+		err = server.ListenAndServe()
 
-	if err != nil {
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(
+		stop,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+
+	<-stop
+
+	log.Println("stopping server")
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("server stopped")
 }
